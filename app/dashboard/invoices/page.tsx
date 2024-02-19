789fdd8a94c345648e8c9f8fd0fc0ead99b99d5d
Rishabhvsc <160497075+Rishabhvsc@users.noreply.github.com>
2024-02-19 20:28:11 +0530

    Delete app directory

--- app/dashboard/invoices/page.tsx
+++
@@ -1,9 +0,0 @@
-import '@/app/ui/global.css';
-export default function page() {
-    return(
-        <>
-        <p>this is invoice page</p>
-        </>
-    )
-    
-}
--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import '@/app/ui/global.css';
-export default function page() {
-    return(
-        <>
-        <p>this is dashboard page</p>
-        </>
-    )
-    
-}
